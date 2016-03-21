ca8372587954da798e2df9d37a35bffefb4d9a2b
Colleen Regan <cfregan@umich.edu>
2016-03-21 00:08:53 -0400

    Corner detection

--- code/linuxBuild/hough_lines.cpp
+++ code/linuxBuild/hough_lines.cpp
@@ -7,10 +7,8 @@
 using namespace cv;
 using namespace std;
 static const Size finalSize(640, 480);
-struct compositeCircle{
-  Vec3i circle;
-  int numCombines;
-};
+
+
 static void help()
 {
   cout << "\nThis program demonstrates line finding with the Hough transform.\n"
@@ -37,27 +35,79 @@ int main(int argc, char** argv)
     return -1;
   }
   
-  Mat src, dst, color_dst;
+  Mat src, dst, dst2, color_dst, mask,tmp,sharp_im;
   if( argc != 2 || !(src=imread(argv[1], 0)).data)
       return -1;
  
-  Canny( src, dst, 50, 200, 3 );
+  Canny( src, dst, 100, 100*3, 3);
   cvtColor( dst, color_dst, CV_GRAY2BGR );
 
+  //resize(src,src,finalSize);
+  //imshow("Original",src);
+
+  //cvtColor(src, src, CV_BGR2GRAY);
+  //blur(src,src,Size(3,3));
+  //adaptiveThreshold(src, src, 100, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY,11,2);
+  //resize(src,src,finalSize);
+  //imshow("Threshold",src);
+  
+/*
+  namedWindow("original",1);
+  resize(src,tmp,finalSize);
+  imshow("original",tmp);
+
+  tmp = 0;
+
+  GaussianBlur(src,tmp,Size(5,5), 5);
+  
+  addWeighted(src, 1.5, tmp, -0.5,0, src);
+  sharp_im = src;
+  namedWindow("Sharpened Image",1);
+  resize(sharp_im, sharp_im, finalSize);
+  imshow("Sharpened Image", sharp_im);
+*/
+
+  sharp_im = src; 
+  equalizeHist(src, sharp_im);
+  
+
+  namedWindow("sharpened image", 1);
+  resize(sharp_im,sharp_im,finalSize);
+  imshow("sharpened image", sharp_im);
+
+  vector<Point2f>corners;
+  goodFeaturesToTrack(src,corners,370, 0.1, 50, mask, 18, 10, 0.1); //qualit = 0.1, min = 50
+
+  for(size_t i = 0; i < corners.size(); i++)
+  {
+	circle(src, corners[i], 10, Scalar(255.), -1);
+  }
+
+  namedWindow("Corners",1);
+  resize(src, src, finalSize);
+  imshow("Corners", src);
+
+
+
+  //namedWindow("Edges",1);
+  //resize(dst,dst2,finalSize);
+  //imshow("Edges",dst2);
+
   vector<Vec4i> lines;
-  HoughLinesP( dst, lines, 1, CV_PI/180, 80, 30, 10 );
+  HoughLinesP( dst, lines, 1, CV_PI/180, 50, 30, 10 );
   for( size_t i = 0; i < lines.size(); i++ )
   {
       line( color_dst, Point(lines[i][0], lines[i][1]),
           Point(lines[i][2], lines[i][3]), Scalar(0,0,255), 3, 8 );
   }
 
-  namedWindow( "Source", 1 );
-  imshow( "Source", src );
+ // namedWindow( "Source", 1 );
+  //resize(src,src,finalSize);
+  //imshow( "Source", src );
 
-  namedWindow( "Detected Lines", 1 );
-  resize(color_dst, color_dst, finalSize);
-  imshow( "Detected Lines", color_dst );
+  //namedWindow( "Detected Lines", 1 );
+  //resize(color_dst, color_dst, finalSize);
+  //imshow( "Detected Lines", color_dst );
 
   waitKey(0);
   return 0;
