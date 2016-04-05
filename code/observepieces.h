e5d74c97cb4bcd931c8ec1b0c0d5da56180eb5e6
johnconn <johnconn@umich.edu>
2016-04-04 20:26:48 -0400

    adding observepieces.h

---
+++ code/observepieces.h
@@ -0,0 +1,9 @@
+#ifndef OBSERVE_PIECES
+#define OBSERVE_PIECES
+#include <string>
+#include "Gomoku.h"
+using namespace cv;
+using namespace std;
+
+vector <compositeCircle> observePieces(string filename);
+#endif
